1f9adb23f54b5ae7c038c3750f41a7a176c2291c
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-15 16:21:24 -0400

    added npm packages to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+let app = express();
