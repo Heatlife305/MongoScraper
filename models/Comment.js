b6f56358ad9e04b26e8132bb9bf221a856e17906
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-23 17:01:25 -0400

    setup article and comment models

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let CommentSchema = new Schema({
+    name: {
+        type: String
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    
+});
+
+let Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
