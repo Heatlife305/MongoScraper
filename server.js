0c9efebb21c490a1f02504ebe7d136cdecf4a593
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-23 16:18:05 -0400

    connected to mongo db

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 let app = express();
+
+app.use(express.static(process.cwd() + "/public"));
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/MongoScraper");
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!");
+});
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log(`Listening on PORT http://localhost:${PORT}`)
+});
