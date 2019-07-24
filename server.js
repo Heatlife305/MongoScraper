ff1a6bf584e43177abdfaf3f10f3afc4f7feeb61
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-23 21:34:13 -0400

    configured all get routes and post routes successfully

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-const express = require("express");
-const mongoose = require("mongoose");
+//dependencies
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-const axios = require("axios");
-const cheerio = require("cheerio");
+//initialize Express app
+var express = require("express");
+var app = express();
 
-let app = express();
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 app.use(express.static(process.cwd() + "/public"));
-
-const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+//Require set up handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/MongoScraper");
-let db = mongoose.connection;
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
-    console.log("Connected to Mongoose!");
+  console.log("Connected to Mongoose!");
 });
 
-let PORT = process.env.PORT || 3000;
-
-app.listen(PORT, function() {
-    console.log(`Listening on PORT http://localhost:${PORT}`)
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+//Create localhost port
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on http://localhost:" + port);
 });
